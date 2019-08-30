19d5a4de2db25997f5459c15b20a8f160303ef40
FUJISE Takumi <fujise@lean-system.co.jp>
2019-08-30 18:23:58 +0900

    AdvancedRoadmap::ProjectsHelperPatch 修正

--- lib/advanced_roadmap/projects_helper_patch.rb
+++ lib/advanced_roadmap/projects_helper_patch.rb
@@ -2,9 +2,16 @@ require_dependency "projects_helper"
 
 module AdvancedRoadmap
   module ProjectsHelperPatch
-    def project_settings_tabs
-      tabs = super
-      index = tabs.index({:name => 'versions', :action => :manage_versions, :partial => 'projects/settings/versions', :label => :label_version_plural})
+    extend ActiveSupport::Concern
+
+    included do
+      alias_method :project_settings_tabs_without_more_tabs, :project_settings_tabs
+      alias_method :project_settings_tabs, :project_settings_tabs_with_more_tabs
+    end
+
+    def project_settings_tabs_with_more_tabs
+      tabs = project_settings_tabs_without_more_tabs
+      index = tabs.index{|h| h[:name] == 'versions' }
       if index
         tabs.insert(index, {:name => "milestones", :action => :manage_milestones, :partial => "projects/settings/milestones", :label => :label_milestone_plural})
         tabs.select {|tab| User.current.allowed_to?(tab[:action], @project)}
